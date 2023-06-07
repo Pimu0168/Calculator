a367d436243b8c0eae69f4086287ded527782da0
Amrin Pimu <134121997+Pimu0168@users.noreply.github.com>
2023-06-08 00:57:26 +0600

    Add files via upload

---
+++ 5.16..sizeof.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+
+    printf("The size of integer is %d\n",sizeof(i));
+    printf("The size of float is %d\n",sizeof(f));
+    printf("The size of double is %d\n",sizeof(d));
+    printf("The size of character is %d\n",sizeof(c));
+
+
+}
